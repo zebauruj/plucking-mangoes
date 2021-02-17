c631685d16b5cd575fa40f96c6bd13554f5b4894
zeba <63093390+zebauruj@users.noreply.github.com>
2021-02-17 13:15:18 +0530

    Add files via upload

---
+++ js/tree.js
@@ -0,0 +1,29 @@
+class tree
+{
+	constructor(x,y)
+	{
+		this.x=x;
+		this.y=y;
+		this.dustbinWidth=450;
+		this.dustbinHeight=600;
+		this.wallThickness=10;
+		
+		this.image=loadImage("images/tree.png")
+		
+		this.body=Bodies.rectangle(x,y,450,10,{isSatic:true});
+		World.add(world, this.body)
+		
+	}
+	display()
+	{
+			var posBottom=this.body.position;
+			push()
+			translate(posBottom.x, posBottom.y+10);
+			fill(255)
+			imageMode(CENTER);
+			image(this.image, 0,-this.dustbinHeight/2,this.dustbinWidth, this.dustbinHeight)
+			pop()
+			
+	}
+
+}
